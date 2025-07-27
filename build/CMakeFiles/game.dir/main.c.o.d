6e1320fd0ada915fc752ccec93d70d5cc7bc4bdc
KillSwitch <benshaies17@gmail.com>
2025-07-26 20:44:03 -0400

    Add .gitignore and clean up build files

--- build/CMakeFiles/game.dir/main.c.o.d
+++
@@ -1,5 +0,0 @@
-CMakeFiles/game.dir/main.c.o: /home/killswitch/SquareDash/main.c \
- /usr/include/stdc-predef.h /home/killswitch/raylib/src/raylib.h \
- /usr/lib/gcc/x86_64-linux-gnu/11/include/stdarg.h \
- /usr/lib/gcc/x86_64-linux-gnu/11/include/stdbool.h \
- /home/killswitch/SquareDash/player.h
